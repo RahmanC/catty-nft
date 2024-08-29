00f488979f153a3fecf7b9230a86ca1c2f9699e7
abdulrahman <abdulrahmanolalekan@enyata.com>
2024-08-29 15:00:05 +0100

    ft:Deploy NFT

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -1,36 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+contract MyNFT is ERC721URIStorage {
+    uint256 private _tokenIds;
 
-    // Base URI for metadata
-    string private _baseTokenURI;
+    constructor() ERC721("MyNFT", "NFT") {}
 
-    constructor(string memory name, string memory symbol, string memory baseTokenURI) ERC721(name, symbol) {
-        _baseTokenURI = baseTokenURI;
-    }
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        
+        returns (uint256)
+    {
+        _tokenIds++;
 
-    // Function to mint new NFTs
-    function mintNFT(address recipient) public onlyOwner returns (uint256) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _safeMint(recipient, newItemId);
+        uint256 newItemId = _tokenIds;
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
         return newItemId;
-    }
-
-    // Override the baseURI function
-    function _baseURI() internal view override returns (string memory) {
-        return _baseTokenURI;
-    }
-
-    // Function to update the base URI (only owner)
-    function setBaseURI(string memory newBaseURI) public onlyOwner {
-        _baseTokenURI = newBaseURI;
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
 import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.20",
+  defaultNetwork: "sepolia",
   networks: {
+    hardhat: {},
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.PROJECT_ID}`,
-      accounts: [process.env.PRIVATE_KEY] as any,
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
     },
   },
 };
-
-export default config;
